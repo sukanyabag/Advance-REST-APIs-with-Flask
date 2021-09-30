3eb09174863da8cf5812f13c0c1df98008bbccec
Sukanya Bag <sukannya472@gmail.com>
2021-09-30 19:16:58 +0530

    added put method to send put req to rest api, tested api response on postman

--- Authentication and Logging In/app.py
+++ Authentication and Logging In/app.py
@@ -19,13 +19,13 @@ class Item(Resource):
     @jwt_required() #decorator
     #note - authenticate 1st in postman before calling get method
     
-    #get request to api
+    #get request to rest api
     def get(self, name):
         #next - returns next item from the iterator
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item' : item}, 200 if item else 404 #status code - 404 not found, 200 OK
     
-    #post request to api
+    #post request to rest api
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400 #status code - 400 bad request
@@ -37,11 +37,28 @@ class Item(Resource):
         items.append(item)
         return item, 201 #status code - 201 created
     
-    #delete request to api
+    #delete request to rest api
     def delete(self, name):
         global items
         items = list(filter(lambda x: x['name'] != name, items))
         return {'message' : 'Item has been successfully deleted.'}
+    
+    #put request to rest api
+    #idempotent request - can call this rquest as many times needed, and o/p never changes
+    #creates as well as updates an existing resource(items)
+    def put(self, name):
+        data = request.get_json()
+        item = next(filter(lambda x: x['name'] == name, items), None)
+        
+        #if no such item, create one
+        if item is None:
+            item = {'name': name, 'price': data['price']}
+            items.append(item)
+        #else, update existing item
+        else:
+            item.update(data)
+        return item
+    
 
 class ItemList(Resource):
     def get(self):
