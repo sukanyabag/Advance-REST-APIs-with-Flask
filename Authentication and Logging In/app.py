110f92036b93d22e82f0adb9999c1aa90badaf12
Sukanya Bag <sukannya472@gmail.com>
2021-09-30 18:51:56 +0530

    added delete request, tested api req in postman

--- Authentication and Logging In/app.py
+++ Authentication and Logging In/app.py
@@ -18,20 +18,30 @@ items = []
 class Item(Resource):
     @jwt_required() #decorator
     #note - authenticate 1st in postman before calling get method
+    
+    #get request to api
     def get(self, name):
         #next - returns next item from the iterator
         item = next(filter(lambda x: x['name'] == name, items), None)
         return {'item' : item}, 200 if item else 404 #status code - 404 not found, 200 OK
-
+    
+    #post request to api
     def post(self, name):
         if next(filter(lambda x: x['name'] == name, items), None) is not None:
             return {'message': "An item with name '{}' already exists.".format(name)}, 400 #status code - 400 bad request
+    
 
         #json payload, silent=True -> returns none, no error msg
         data = request.get_json(silent=True)
         item = {'name': name, 'price':data['price']}
         items.append(item)
         return item, 201 #status code - 201 created
+    
+    #delete request to api
+    def delete(self, name):
+        global items
+        items = list(filter(lambda x: x['name'] != name, items))
+        return {'message' : 'Item has been successfully deleted.'}
 
 class ItemList(Resource):
     def get(self):
