054e1b608d3a9c3455d35a98837aad4d24f4d427
Sukanya Bag <sukannya472@gmail.com>
2022-02-23 12:46:47 +0530

    added type hinting

--- Advance ECommerce Store API/Token Refreshment & JWT extended/models/store.py
+++ Advance ECommerce Store API/Token Refreshment & JWT extended/models/store.py
@@ -1,3 +1,4 @@
+from typing import Dict, List
 from database import db
 
 class StoreModel(db.Model):    # tells SQLAlchemy that it is something that will be saved to database and will be retrieved from database
@@ -6,14 +7,14 @@ class StoreModel(db.Model):    # tells SQLAlchemy that it is something that will
 
   # Columns
   id = db.Column(db.Integer, primary_key=True)
-  name = db.Column(db.String(80))
+  name = db.Column(db.String(80), unique = True)
 
   items = db.relationship("ItemModel", lazy="dynamic")  # this will allow us to check which item is in store whose id is equal to it's id.
                                       # lazy="dynamic" tells sqlalchemy to not create seperate objects for each item that is created
-  def __init__(self, name):
+  def __init__(self, name: str):
     self.name = name
 
-  def json(self):
+  def json(self) -> Dict:
     return {
       "id": self.id, 
       "name": self.name, 
@@ -22,20 +23,20 @@ class StoreModel(db.Model):    # tells SQLAlchemy that it is something that will
 
   # searches the database for items using name
   @classmethod
-  def find_item_by_name(cls, name):
+  def find_store_by_name(cls, name: str):
     # return cls.query.filter_by(name=name) # SELECT name FROM __tablename__ WHERE name=name
     # this function would return a StoreModel object
     return cls.query.filter_by(name=name).first() # SELECT name FROM __tablename__ WHERE name=name LIMIT 1
 
   @classmethod
-  def find_all(cls):
+  def find_all(cls) -> List:
     return cls.query.all()
 
   # method to insert or update an item into database
-  def save_to_database(self):
+  def save_to_database(self) -> None:
     db.session.add(self)  # session here is a collection of objects that wil be written to database
     db.session.commit()
 
-  def delete_from_database(self):
+  def delete_from_database(self) -> None:
     db.session.delete(self)
     db.session.commit()
